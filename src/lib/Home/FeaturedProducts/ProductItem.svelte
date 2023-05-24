dd318ce59791a612cbccda4d7a5c5293b6f846d9
Vinh Chiem <trieuvinb@gmail.com>
2023-05-24 15:17:39 +0700

    Update href

--- src/lib/Home/FeaturedProducts/ProductItem.svelte
+++ src/lib/Home/FeaturedProducts/ProductItem.svelte
@@ -10,13 +10,13 @@
     style="background-image: url(&quot;{image}&quot;);"
   >
     <ul class="featured__item__pic__hover">
-      <li><a href="#"><i class="fa fa-heart"></i></a></li>
-      <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+      <li><a href="/"><i class="fa fa-heart"></i></a></li>
+      <li><a href="/"><i class="fa fa-retweet"></i></a></li>
+      <li><a href="/"><i class="fa fa-shopping-cart"></i></a></li>
     </ul>
   </div>
   <div class="featured__item__text">
-    <h6><a href="#">{title}</a></h6>
+    <h6><a href="/">{title}</a></h6>
     <h5>${price}</h5>
   </div>
 </div>
