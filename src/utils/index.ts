d3aa95f6b2149384428ad87cd3189a2b531fb5b0
Vinh Chiem <trieuvinb@gmail.com>
2023-06-12 15:01:59 +0700

    [ORGANI] remove unused files

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import { onDestroy } from 'svelte';
-
 export const exFuntion = () => {
   return '';
 };
@@ -12,27 +10,3 @@ export function sliceIntoChunks<T>(arr: T[], chunkSize: number): Array<Array<T>>
   }
   return res;
 }
-
-export const secondsToTime = (secs: number): { [x: string]: number } => {
-  secs = Math.round(secs);
-  const hours = Math.floor(secs / (60 * 60));
-
-  const divisor_for_minutes = secs % (60 * 60);
-  const minutes = Math.floor(divisor_for_minutes / 60);
-
-  const divisor_for_seconds = divisor_for_minutes % 60;
-  const seconds = Math.ceil(divisor_for_seconds);
-
-  const obj = {
-    h: hours,
-    m: minutes,
-    s: seconds
-  };
-  return obj;
-};
-
-export const onInterval = (cb: () => void, timer: number) => {
-  const interval = setInterval(cb, timer);
-
-  onDestroy(() => clearInterval(interval));
-};
