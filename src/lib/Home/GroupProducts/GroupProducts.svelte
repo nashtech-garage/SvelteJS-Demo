01fc9f5d364992b727263b552a4820e4e4939aaf
rinto <rinto0503@gmail.com>
2023-06-27 13:52:49 +0700

    Intagrate for lasted product

--- src/lib/Home/GroupProducts/GroupProducts.svelte
+++ src/lib/Home/GroupProducts/GroupProducts.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
   import ProductOrder from '../../../lib/Components/ProductOrder/ProductOrder.svelte';
   import { GroupLatestProducts } from '../../../constants/home';
-  import type { TGroupProductItem } from '../../../types/home';
-  const itemGenerator = (): TGroupProductItem[] => {
-    const newArray = [1, 2, 3, 1, 2, 3];
-    return [...newArray].map((number, index) => {
-      const order = index + 1;
-      return {
-        image: `./images/LatestProducts/lp-${number}.jpg`,
-        title: `${order}`,
-        price: 30
-      };
-    });
+  import { GroupProductValues, type TGroupProductItem } from '../../../types/home';
+  export let lastedProduct: any[] = [];
+  export let topRateProduct: any[] = [];
+  export let reviewedProduct: any[] = [];
+
+  const lastedProductList = (title: string): TGroupProductItem[] => {
+    switch (title) {
+      case GroupProductValues.LATEST_PRODUCTS:
+        return buildListItem(lastedProduct)
+      case GroupProductValues.TOP_RATED_PRODUCTS:
+        return buildListItem(topRateProduct)
+      default:
+        return buildListItem(reviewedProduct)
+    }
+    
   };
-  const LatestList = GroupLatestProducts.map((item) => {
+
+  const buildListItem = (list: any) => list.map((item: any) => ({
+    image: item.list_img[0].url,
+    title: item.name,
+    price: item.price,
+  }));
+  
+  const groupItemList = GroupLatestProducts.map((item) => {
     return {
       title: item,
-      productList: itemGenerator()
+      productList: lastedProductList(item)
     };
   });
 </script>
@@ -24,9 +35,9 @@
 <section class="latest-product spad">
   <div class="container">
     <div class="row">
-      {#each LatestList as item (item.title)}
+      {#each groupItemList as item (item.title)}
         <div class="col-lg-4 col-md-6">
-          <ProductOrder title="{item.title}" productList="{item.productList}" />
+          <ProductOrder title="{item.title}" productList={item.productList} />
         </div>
       {/each}
     </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,6 +3,10 @@ import Api from "../servies";
 /** @type {import('./$types').PageLoad} */
 // export const ssr = false;
 export const load = async () => {
-  const response = await Api.get("/api/rest/product/featured_products?limit=10");
-  return { featuredProductList: response.product };
+  const featuredProductRequest = Api.get("/api/rest/api/rest/product/featured_products?limit=100");
+  const lastedProductRequest = Api.get("/api/rest/product/lasted_products?limit=100");
+  const topRateProductRequest = Api.get("/api/rest/product/rated_products?limit=100");
+  const reviewedProductRequest =  Api.get("/api/rest/product/reviewed_products?limit=100");
+  const data = await Promise.all([featuredProductRequest, lastedProductRequest, topRateProductRequest, reviewedProductRequest])
+  return { featuredProductList: data[0].product,  lastedProduct: data[1].product, topRateProduct: data[2].product, reviewedProduct: data[3].product};
 }
